1ec1a58370bb464fe6456baf69f08b1c305f908b
Anuj Negi <anujnegi9997@gmail.com>
2026-04-07 15:40:37 +0530

    bugs fixing

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -31,7 +31,7 @@ const ProfilePage = () => {
   }
 
   return (
-    <div className='min-h-screen bg-coveer bg-no-repeat flex items-center justify-center'>
+    <div style={{ backgroundImage: `url(${assets.bgImage})` }} className='min-h-screen bg-coveer bg-no-repeat flex items-center justify-center'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
         <form onSubmit={handlesubmit} className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile details</h3>
